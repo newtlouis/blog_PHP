ef86459e17d7e80e3e56363d35fd8feb5ecc1541
Louis <louis.m.newton@hotmail.fr>
2021-09-03 22:22:49 +0200

    pagination fonctionnement

--- views/post/index.php
+++ views/post/index.php
@@ -7,8 +7,22 @@ use App\Model\Post;
 
 $title = 'Mon blog';
 
-$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
+// Pagination
+$currentPage = (int)($_GET['page'] ?? 1 );
+if ($currentPage <= 0){
+    throw new Exception('Numéro de page invalide');
+}
+$count = (int)($pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0]);
+$perPage = 12;
+$pages = ceil($count/$perPage);
+if ($currentPage > $pages){
+    throw new Exception('Cette page n\'existe pas');
+}
+
 // On récupère les datas et on les insère dans la class Post
+$offset = $perPage *($currentPage - 1);
+// dd($offset);
+$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
 $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 
 ?>
