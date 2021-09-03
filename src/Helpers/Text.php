15f1b061b9560140d6a0ce61bf0b644072553e03
Louis <louis.m.newton@hotmail.fr>
2021-09-03 17:53:07 +0200

    Tronquer les articles dans le listing

--- src/Helpers/Text.php
+++ src/Helpers/Text.php
@@ -3,7 +3,16 @@
 namespace App\Helpers;
 
 class Text {
-    
+
+    public static function excerpt( string $content, int $limit=60 , string $mode="..."): string{
+            // mb_strlen compte la longueur de la chaine de charactères avec les emojis (mb_)
+        if (mb_strlen($content) <= $limit){
+            return $content;
+        }
+    // Pour ne pas couper en plein milieu d'un mot, on cherche le dernier espace avec la limite de charactères et emojis (mb_) et on tronque à cet endroit
+    $lastSpace = mb_strpos($content , ' ' , $limit );
+    return mb_substr( $content , $lastSpace ) . $mode;
+    }
 }
 
 ?>
--- views/post/index.php
+++ views/post/index.php
@@ -1,5 +1,10 @@
 <?php 
+// importation base de donnée
 require dirname(dirname(__DIR__)) . '/db/db.php';
+
+// Importation namespace Text
+use App\Helpers\Text;
+
 $title = 'Mon blog';
 
 $query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
@@ -17,7 +22,7 @@ $posts = $query->fetchAll(PDO::FETCH_OBJ);
             <div class="card-body">
                 <h5 class="card-title"> <?= htmlentities( $post->name ) ?> </h5>
                 <!-- nl2br pour respecter les paragraphes -->
-                <p><?= nl2br(htmlentities($post->content)) ?></p>
+                <p><?= nl2br(htmlentities(Text::excerpt($post->content))) ?></p>
                 <p>
                     <a href="#" class="btn btn-primary">Voir plus</a>
                 </p>
